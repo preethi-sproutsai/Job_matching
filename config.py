f3e75d10ecb17a3cb45cf4b7c628f099ce028619
preethi-sproutsai <preethi@sproutsai.dev>
2025-09-08 17:20:49 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+from pathlib import Path
+
+EXCHANGE_RATE_FILE = Path("inr_usd_rate.json")
+EXCHANGE_API_KEY = "fca_live_Mz2e64CuQ8CpmQueVp0SC12rPTAMOgTHhSeEurCv"
+LAST_UPDATE_FILE = Path("last_updated_at.json")
+SPR_OUTS_API_URL = "http://sprouts-service/api/jobs"
